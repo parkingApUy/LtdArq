76ecc5d5ba004339382edeff4ab90079947b7501
parkingApUy <parkingapuy@gmail.com>
2018-01-15 16:30:07 -0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,22 +1,34 @@
 <?php
 
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+//Load composer's autoloader
+require 'vendor/autoload.php';
 
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+$mail->isSMTP(); 
+$mail->Host       = "smtp.zoho.com"; // SMTP server example
+$mail->SMTPAuth   = true;                  // enable SMTP authentication
+$mail->Port       = 465;                    // set the SMTP port for the GMAIL server
+$mail->SMTPSecure = 'SSL';  
+$mail->Username   = "info@parkingap"; // SMTP account username example
+$mail->Password   = "fabrizio1717";   
 
-    // Enter the email where you want to receive the message
-    $emailTo = 'info@parkingap.com.uy';
+$mail->setFrom('from@example.com', 'Mailer');
+$mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $clientEmail = addslashes(trim($_POST['email']));
-    $message = addslashes(trim($_POST['message']));
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
 
-    	$headers = "From: " . $clientEmail . " <" . $clientEmail . ">" . "\r\n" . "Reply-To: " . $clientEmail;
-		mail($emailTo, $message, $headers);
-	
-    
-	
-	
-    
 
 
 

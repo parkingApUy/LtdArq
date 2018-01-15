4885f39e0c63ef5bf03bd54a36d1c714bc2086eb
parkingApUy <parkingapuy@gmail.com>
2018-01-15 18:21:28 -0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,9 +5,11 @@ require("sendgrid-php/sendgrid-php.php");
 
 
 $from = new SendGrid\Email(null, "clientEmail@gmail.com");
+$subject = "LTD+";
 $to = new SendGrid\Email(null, "info@parkingap.com.uy");
 $content = new SendGrid\Content("text/plain", "message");
-$mail = new SendGrid\Mail($from, $to, $content);
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
 
 $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);

ef6e33e7f9ad46e24e2819c5034dead12217d7f3
parkingApUy <parkingapuy@gmail.com>
2018-01-15 16:50:54 -0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,35 +1,18 @@
 <?php
 
+require("SendGrid.php");
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+$from = new SendGrid\Email(null, "test@example.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "info@parkingap.com.uy");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-//Load composer's autoloader
-require 'vendor/autoload.php';
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-$mail->isSMTP(); 
-$mail->Host       = "smtp.zoho.com"; // SMTP server example
-$mail->SMTPAuth   = true;                  // enable SMTP authentication
-$mail->Port       = 465;                    // set the SMTP port for the GMAIL server
-$mail->SMTPSecure = 'SSL';  
-$mail->Username   = "info@parkingap"; // SMTP account username example
-$mail->Password   = "fabrizio1717";   
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
 
-$mail->setFrom('from@example.com', 'Mailer');
-$mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-}
-
-
-
-
-?>

42f7240e307d6340556577fcdf43586a3952ba8f
parkingApUy <parkingapuy@gmail.com>
2018-01-16 15:49:38 -0300

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -302,6 +302,51 @@ jQuery(document).ready(function() {
 	});
 });
 
+/*
+    Contact form
+	*/
+	$('.contact-form form input[type="text"], .contact-form form textarea').on('focus', function() {
+		$('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
+	});
+	$('.contact-form form').submit(function(e) {
+		e.preventDefault();
+	    $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
+	    var postdata = $('.contact-form form').serialize();
+	    $.ajax({
+	        type: 'POST',
+	        url: 'contact.php',
+	        data: postdata,
+	        dataType: 'json',
+	        success: function(json) {
+	            if(json.emailMessage != '') {
+	                $('.contact-form form .contact-email').addClass('contact-error animated shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            			$(this).removeClass('animated shake');
+            		});
+	            }
+	            
+	            if(json.messageMessage != '') {
+	                $('.contact-form form textarea').addClass('contact-error animated shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            			$(this).removeClass('animated shake');
+            		});
+	            }
+	            if(json.emailMessage == '' && json.messageMessage == '') {
+	                $('.contact-form form').fadeOut('fast', function() {
+	                    $('.contact-form').append('<p>Gracias por contactarnos!</p>');
+	                });
+	            }
+	        }
+	    });
+	});
+    
+});
+
+
+
+
+
+
+
+
 
 
 

873c76b9d7a5562d262eb415e6982f0b0db1a11f
parkingApUy <parkingapuy@gmail.com>
2018-01-17 16:13:16 -0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -16,8 +16,6 @@ $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
+
 
 

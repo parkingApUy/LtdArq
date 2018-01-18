7bee5ec2218dab2f43c5640b5701e832a50062e5
parkingApUy <parkingapuy@gmail.com>
2018-01-18 18:27:04 -0300

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -326,8 +326,7 @@ $(function() {
 		})
 		.done(function(response) {
 			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
+			
 
 			// Set the message text.
 			$(formMessages).text(response);
@@ -339,18 +338,7 @@ $(function() {
 			//$('#email').val('');
 			//$('#message').val('');
 		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
-
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(email);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
-		});
+		
 
 	});
 

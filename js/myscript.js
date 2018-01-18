5f5f1ff4cd2b6a62656a03ab1e51bb357136eff3
parkingApUy <parkingapuy@gmail.com>
2018-01-18 17:07:46 -0300

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -302,33 +302,54 @@ jQuery(document).ready(function() {
 	});
 });
 
-$('#contactForm').submit(function(event){
-    // cancels the form submission
-    event.preventDefault();
-    submitForm();
+$(function() {
+
+	// Get the form.
+	var form = $('#fc');
+
+	// Get the messages div.
+	var formMessages = $('#fields');
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			//$('#name').val('');
+			//$('#email').val('');
+			//$('#message').val('');
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			}
+		});
+
+	});
+
 });
-
-
-function submitForm(){
-    // Initiate Variables With Form Content
-    var email = $("#email").val();
-    var message = $("#message").val();
- 
- $.ajax({
-	        type: 'POST',
-	        url: 'contact.php',
-	        data: postdata,
-	        dataType: 'json',
-	        success: function(json) {
-       if(json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
-	               $( "#msgOculto" ).removeClass( "hidden" );
-	            }
-            
-        
-		
-	  } 
-	  })
-}
-	
-	
-
